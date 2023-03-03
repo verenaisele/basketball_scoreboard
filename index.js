73ea41f51eb31e313fff06a5f0823cedd7f035c8
verenaisele <77627438+verenaisele@users.noreply.github.com>
2023-03-03 13:36:10 +0100

    Change GUEST to AWAY

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-let scoreHome = document.getElementById("score-home")
-let scoreGuest = document.getElementById("score-guest")
-let homePoints = 0
-let guestPoints = 0
+let scoreHome = document.getElementById('score-home');
+let scoreGuest = document.getElementById('score-guest');
+let homePoints = 0;
+let guestPoints = 0;
 
-function add1() {
-    homePoints += 1
-    scoreHome.textContent = homePoints
+function add1ToHome() {
+  homePoints += 1;
+  scoreHome.textContent = homePoints;
 }
 
-function add2() {
-    homePoints += 2
-    scoreHome.textContent = homePoints
+function add2ToHome() {
+  homePoints += 2;
+  scoreHome.textContent = homePoints;
 }
 
-function add3() {
-    homePoints += 3
-    scoreHome.textContent = homePoints
+function add3ToHome() {
+  homePoints += 3;
+  scoreHome.textContent = homePoints;
+}
+
+function add1ToGuest() {
+  guestPoints += 1;
+  scoreGuest.textContent = guestPoints;
+}
+
+function add2ToGuest() {
+  guestPoints += 2;
+  scoreGuest.textContent = guestPoints;
+}
+
+function add3ToGuest() {
+  guestPoints += 3;
+  scoreGuest.textContent = guestPoints;
 }
